89d48b4b2cab6afcffda65bde23df18403752ee8
mapulre <151844877+mapulre@users.noreply.github.com>
2024-04-20 21:05:36 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     },
     author="rycont",
     author_email="rycont@outlook.kr",
-    description="어떻게 엄준식이 언어이름이냐🤣",
+    description="어떻게 엄준식이 언어이름이냐",
     long_description=open("../README.md", "r", encoding="UTF-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/rycont/umjunsik-lang",
@@ -22,6 +22,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.12.2",
     ],
 )
